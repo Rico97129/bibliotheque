f0eb6ea780ca0fd9a28bf655531239af6cbcadcd
Rico97129 <richardson.thicot@outlook.fr>
2023-02-20 14:23:23 +0100

    Crud

---
+++ src/Entity/Ouvrage.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OuvrageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OuvrageRepository::class)]
+class Ouvrage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $titre = null;
+
+    #[ORM\ManyToMany(targetEntity: Auteur::class)]
+    private Collection $auteur;
+
+    public function __construct()
+    {
+        $this->auteur = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Auteur>
+     */
+    public function getAuteur(): Collection
+    {
+        return $this->auteur;
+    }
+
+    public function addAuteur(Auteur $auteur): self
+    {
+        if (!$this->auteur->contains($auteur)) {
+            $this->auteur->add($auteur);
+        }
+
+        return $this;
+    }
+
+    public function removeAuteur(Auteur $auteur): self
+    {
+        $this->auteur->removeElement($auteur);
+
+        return $this;
+    }
+}
